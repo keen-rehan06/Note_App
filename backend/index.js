342d94a4188af936c4bbb664d8d27ecbdf997e1b
Rehan Sheikh <rehansheikh0808@gmail.com>
2026-02-21 12:19:46 +0530

    create a server

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import cookieParser from "cookie-parser";
+import express from "express"
+
+const app = express();
+
+app.use(express.json({}))
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+
+app.get("/",function(req,res){
+    console.log("Running")
+    res.send("Running")
+})
+
+app.listen(3000,function(){
+    console.log("App is runnig on port 3000")
+})
